f8ef9b38bbc1c80355f1bb6b1645e1e5a133ad10
mark medlin <mmedlin1997@yahoo.com>
2019-02-28 11:56:13 -0800

    Minor cleanup

--- src/main/java/cs5850/CS5850_P1_Dropbox/App.java
+++ src/main/java/cs5850/CS5850_P1_Dropbox/App.java
@@ -1,7 +1,5 @@
 package cs5850.CS5850_P1_Dropbox;
 
-import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -47,79 +45,79 @@ public class App
        System.out.println( "Program Done" );
     }
     
-    public static void s3client() {
-       final String BUCKET_NAME = "mmedlin-test";
-//       final String BUCKET_NAME = "mmedlin-dropbox-uswest1";
-       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\";
-       final String FILE1 = "test1.txt";
-       final String FILE2 = "test1_copy.txt";
-       final String FILE3 = "test3.txt";
-       final String FILE4 = "test3_modify.txt";
-       
-       // Setup using credentials at ~/user/.aws
-       AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-       IAwsS3Client s3 = new AwsS3Client(s3Client, BUCKET_NAME);
-       
-       System.out.println("Bucket list:");
-       s3.printList();
-       
-       // Put files to S3
-       System.out.println("After adding file:");
-       s3.putFile(new File(DROPBOX_PATH + FILE1));
-       s3.printList();
-       
-       System.out.println("After copying file:");
-       s3.copyFile(FILE1, FILE2);
-       s3.printList();
-       
-       System.out.println("After deleting file:");
-       s3.deleteFile(FILE1);
-       s3.printList();
-       
-       System.out.println("After renaming file:");
-       s3.putFile(new File(DROPBOX_PATH + FILE3));
-       s3.renameFile(FILE3, FILE4);
-       s3.printList();
-    }
+//    public static void s3client() {
+//       final String BUCKET_NAME = "mmedlin-test";
+////       final String BUCKET_NAME = "mmedlin-dropbox-uswest1";
+//       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\";
+//       final String FILE1 = "test1.txt";
+//       final String FILE2 = "test1_copy.txt";
+//       final String FILE3 = "test3.txt";
+//       final String FILE4 = "test3_modify.txt";
+//       
+//       // Setup using credentials at ~/user/.aws
+//       AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+//       IAwsS3Client s3 = new AwsS3Client(s3Client, BUCKET_NAME);
+//       
+//       System.out.println("Bucket list:");
+//       s3.printList();
+//       
+//       // Put files to S3
+//       System.out.println("After adding file:");
+//       s3.putFile(new File(DROPBOX_PATH + FILE1));
+//       s3.printList();
+//       
+//       System.out.println("After copying file:");
+//       s3.copyFile(FILE1, FILE2);
+//       s3.printList();
+//       
+//       System.out.println("After deleting file:");
+//       s3.deleteFile(FILE1);
+//       s3.printList();
+//       
+//       System.out.println("After renaming file:");
+//       s3.putFile(new File(DROPBOX_PATH + FILE3));
+//       s3.renameFile(FILE3, FILE4);
+//       s3.printList();
+//    }
     
-    public static void watchFolder() {
-       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox";
-       Path path = Paths.get(DROPBOX_PATH);
-       
-       // Setup using credentials at ~/user/.aws
-       AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-       IAwsS3Client s3 = new AwsS3Client(s3Client, "mmedlin-test");
-       
-       Thread fwt = new Thread(new FolderWatcher(path, s3));
-       fwt.start();
-       
-       Scanner in = new Scanner(System.in);
-       if(in.nextLine().isEmpty()) {
-          fwt.interrupt();
-       } else {
-          fwt.interrupt();
-       }
-       in.close();
-    }
+//    public static void watchFolder() {
+//       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox";
+//       Path path = Paths.get(DROPBOX_PATH);
+//       
+//       // Setup using credentials at ~/user/.aws
+//       AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+//       IAwsS3Client s3 = new AwsS3Client(s3Client, "mmedlin-test");
+//       
+//       Thread fwt = new Thread(new FolderWatcher(path, s3));
+//       fwt.start();
+//       
+//       Scanner in = new Scanner(System.in);
+//       if(in.nextLine().isEmpty()) {
+//          fwt.interrupt();
+//       } else {
+//          fwt.interrupt();
+//       }
+//       in.close();
+//    }
     
-    public static void folderUtility() {
-       final String WATCHED_FOLDER_NAME_1 = "Dropbox";
-       
-       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\";
-       
-       FolderUtility util_1 = new FolderUtility(WATCHED_FOLDER_NAME_1);
-       util_1.setPath(Paths.get(DROPBOX_PATH));
-       System.out.println("Watching path: " + util_1.getPath().toString());
-       System.out.println("Is valid? " + util_1.getIsValidPath());
-       
-       final String DROPBOX_PATH_2 = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\test1.txt";
-       util_1.setPath(Paths.get(DROPBOX_PATH_2));
-       System.out.println("Watching path: " + util_1.getPath().toString());
-       System.out.println("Is valid? " + util_1.getIsValidPath());
-       
-       final String DROPBOX_PATH_3 = "C:\\Users\\vanessa\\Desktop\\Java\\junk\\";
-       util_1.setPath(Paths.get(DROPBOX_PATH_3));
-       System.out.println("Watching path: " + util_1.getPath().toString());
-       System.out.println("Is valid? " + util_1.getIsValidPath());
-    }
+//    public static void folderUtility() {
+//       final String WATCHED_FOLDER_NAME_1 = "Dropbox";
+//       
+//       final String DROPBOX_PATH = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\";
+//       
+//       FolderUtility util_1 = new FolderUtility(WATCHED_FOLDER_NAME_1);
+//       util_1.setPath(Paths.get(DROPBOX_PATH));
+//       System.out.println("Watching path: " + util_1.getPath().toString());
+//       System.out.println("Is valid? " + util_1.getIsValidPath());
+//       
+//       final String DROPBOX_PATH_2 = "C:\\Users\\vanessa\\Desktop\\Java\\Dropbox\\test1.txt";
+//       util_1.setPath(Paths.get(DROPBOX_PATH_2));
+//       System.out.println("Watching path: " + util_1.getPath().toString());
+//       System.out.println("Is valid? " + util_1.getIsValidPath());
+//       
+//       final String DROPBOX_PATH_3 = "C:\\Users\\vanessa\\Desktop\\Java\\junk\\";
+//       util_1.setPath(Paths.get(DROPBOX_PATH_3));
+//       System.out.println("Watching path: " + util_1.getPath().toString());
+//       System.out.println("Is valid? " + util_1.getIsValidPath());
+//    }
 }
