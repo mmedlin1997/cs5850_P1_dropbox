4128c732e5c802cdfdc307de43b638f7016a1eb8
mark medlin <mmedlin1997@yahoo.com>
2019-03-01 00:09:31 -0800

    Minor cleanup

--- src/test/java/cs5850/CS5850_P1_Dropbox/AwsS3ClientTest.java
+++ src/test/java/cs5850/CS5850_P1_Dropbox/AwsS3ClientTest.java
@@ -5,13 +5,11 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
